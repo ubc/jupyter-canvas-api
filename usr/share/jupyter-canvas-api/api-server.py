3654fe303f3a2fb794ad9fb3f78840913146fce0
Rahim Khoja <rahim@khoja.ca>
2022-01-04 13:47:44 -0800

    Update api-server.py

--- usr/share/jupyter-canvas-api/api-server.py
+++ usr/share/jupyter-canvas-api/api-server.py
@@ -100,7 +100,7 @@ def slugify(value, allow_unicode=False):
 def not_authorized(e):
     """ Response sent back when not authorized. """
     if request.headers.getlist("X-Forwarded-For"):
-        client_ip_address = str(request.headers.getlist("X-Forwarded-For").split(','))
+        client_ip_address = str(request.headers.getlist("X-Forwarded-For"))
     else:
         client_ip_address = request.remote_addr
     logger.error("Invalid Authentication from IP: "+str(client_ip_address))
